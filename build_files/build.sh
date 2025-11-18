b085f90ae2897b8f91313698480981bc2a418c37
Ishan Agarwal <ishan365.ia@gmail.com>
2025-11-18 23:39:27 +0530

    add and remove some packages.

--- build_files/build.sh
+++ build_files/build.sh
@@ -11,11 +11,12 @@ set -ouex pipefail
 
 # this installs a package from fedora repos
 dnf5 install -y tmux
-dnf5 install -y just
-dnf5 install -y ninja-build cmake gcc make gettext curl glibc-gconv-extra git clang
+dnf5 install -y ninja-build cmake gcc make gettext curl glibc-gconv-extra git clang python stow
 dnf5 -y copr enable solopasha/hyprland
-dnf5 install -y sddm hyprland hyprpolkitagent hyprcursor dunst pipewire wireplumber xdg-desktop-portal-hyprland
+dnf5 install -y sddm hyprland hyprpolkitagent hyprcursor mako pipewire wireplumber xdg-desktop-portal-hyprland waybar nautilus udiskie iwgtk \
+	gvfs gvfs-afc gvfs-afp gvfs-archive gvfs-client gvfs-fuse gvfs-goa gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb
 dnf5 -y group install virtualization
+dnf5 install -y steam-devices
 
 # Use a COPR Example:
 #
@@ -24,7 +25,10 @@ dnf5 -y group install virtualization
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
 
+dnf5 -y copr disable solopasha/hyprland
+
 #### Example for enabling a System Unit File
 
 systemctl enable sddm
+systemctl enable libvirtd
 systemctl enable podman.socket
