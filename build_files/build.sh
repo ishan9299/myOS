436ab5e4c1939f8295969bdb1179a4ae25c4cc98
Ishan Agarwal <ishan365.ia@gmail.com>
2025-11-19 17:16:53 +0530

    compile and install scroll window manager.

--- build_files/build.sh
+++ build_files/build.sh
@@ -21,13 +21,24 @@ dnf5 install -y meson ninja-build cmake gcc make gettext curl glibc-gconv-extra 
 # scroll compiling
 dnf5 install -y meson wayland-devel mesa-libEGL-devel mesa-libGLES-devel mesa-dri-drivers xorg-x11-server-Xwayland libdrm-devel libgbm-devel libxkbcommon-devel libudev-devel pixman-devel libinput-devel libevdev-devel systemd-devel cairo-devel libpcap-devel json-c-devel pam-devel pango-devel pcre-devel gdk-pixbuf2-devel hwdata-devel
 dnf5 install -y meson wayland-devel wayland-protocols-devel mesa-libEGL-devel mesa-libGLES-devel vulkan-loader-devel vulkan-headers libdrm-devel mesa-libgbm-devel libinput-devel libxkbcommon-devel systemd-devel pixman-devel libseat-devel hwdata libdisplay-info-devel libliftoff-devel glslang lua lua-devel
+cd /tmp
 git clone --depth=1 https://github.com/dawsers/scroll
 cd scroll
 # git clone https://gitlab.freedesktop.org/wlroots/wlroots.git subprojects/wlroots
-mkdir -p /usr/local/bin/
+# mkdir -p /usr/local/bin/
 meson setup build/
 ninja -C build/
-ninja -C build/ install
+cp build/sway/scroll /usr/bin/scroll
+cp build/swaymsg/scrollmsg /usr/bin/scrollmsg
+cp build/swaybar/scrollbar /usr/bin/scrollbar
+cp build/swaynag/scrollnag /usr/bin/scrollnag
+mkdir -p /etc/scroll/ && cp build/config /etc/scroll/config
+cp scroll.desktop /usr/share/wayland-sessions/scroll.desktop
+cp completions/bash/scroll /usr/share/bash-completion/completions/scroll
+cp completions/bash/scrollmsg /usr/share/bash-completion/completions/scrollmsg
+cp completions/bash/scrollbar /usr/share/bash-completion/completions/scrollbar
+# cp build/config
+# ninja -C build/ install
 cd ..
 rm -rf scroll
 
