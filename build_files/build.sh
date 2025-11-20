2687e04db32907ea535f8beeca0c1f80ee3144ef
ishan9299 <47824004+ishan9299@users.noreply.github.com>
2025-11-20 20:18:14 +0530

    Update build.sh to remove walker from install list
    Removed 'tmux' from the installation command in build.sh.

--- build_files/build.sh
+++ build_files/build.sh
@@ -67,7 +67,7 @@ cp completions/bash/scrollbar /usr/share/bash-completion/completions/scrollbar
 cd ..
 rm -rf scroll
 
-dnf5 install -y qemu libvirt steam-devices sddm tmux kitty wmenu-run mesa-dri-drivers polkit swaybg xorg-x11-server-Xwayland grim walker wofi
+dnf5 install -y qemu libvirt steam-devices sddm tmux kitty mesa-dri-drivers polkit swaybg xorg-x11-server-Xwayland grim wofi
 
 dnf5 remove -y firefox
 
